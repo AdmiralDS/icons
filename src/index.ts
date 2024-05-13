a6226776488128c51481349eabd748e260d3db79
Larisa Markova <lovemirror@rambler.ru>
2024-05-13 12:27:12 +0300

    Добавлен экспорт категории logo

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from './icons/documents';
 export * from './icons/finance';
 export * from './icons/flags';
 export * from './icons/location';
+export * from './icons/logo';
 export * from './icons/payment';
 export * from './icons/redact';
 export * from './icons/security';
