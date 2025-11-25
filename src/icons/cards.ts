2c6f650a2621f460883d07953b423a0cf779307b
Larisa Markova <lovemirror@rambler.ru>
2025-11-25 17:15:45 +0300

    Remove redundant artifacts

--- src/icons/cards.ts
+++
@@ -1,9 +0,0 @@
-export { default as CardsBroker } from '@admiral-ds/icons/build/cards/Broker.svg?react';
-export { default as CardsIIS } from '@admiral-ds/icons/build/cards/IIS.svg?react';
-export { default as CardsPaymentsMaster } from '@admiral-ds/icons/build/cards/PaymentsMaster.svg?react';
-export { default as CardsPaymentsMasterEUR } from '@admiral-ds/icons/build/cards/PaymentsMasterEUR.svg?react';
-export { default as CardsPaymentsMasterGBR } from '@admiral-ds/icons/build/cards/PaymentsMasterGBR.svg?react';
-export { default as CardsPaymentsMasterJPY } from '@admiral-ds/icons/build/cards/PaymentsMasterJPY.svg?react';
-export { default as CardsPaymentsMasterRUB } from '@admiral-ds/icons/build/cards/PaymentsMasterRUB.svg?react';
-export { default as CardsPaymentsMasterUAH } from '@admiral-ds/icons/build/cards/PaymentsMasterUAH.svg?react';
-export { default as CardsPaymentsMasterUSD } from '@admiral-ds/icons/build/cards/PaymentsMasterUSD.svg?react';
--- src/icons/location.ts
+++
@@ -1,6 +0,0 @@
-export { default as LocationGPSOutline } from '@admiral-ds/icons/build/location/GPSOutline.svg?react';
-export { default as LocationGPSSolid } from '@admiral-ds/icons/build/location/GPSSolid.svg?react';
-export { default as LocationLocationOutline } from '@admiral-ds/icons/build/location/LocationOutline.svg?react';
-export { default as LocationLocationSolid } from '@admiral-ds/icons/build/location/LocationSolid.svg?react';
-export { default as LocationMapLocationOutline } from '@admiral-ds/icons/build/location/MapLocationOutline.svg?react';
-export { default as LocationMapLocationSolid } from '@admiral-ds/icons/build/location/MapLocationSolid.svg?react';
